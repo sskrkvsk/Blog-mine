3224766e16d4c17d7fb75e9959649d07a2909fd0
ssks <s99ruy@gmail.com>
2023-09-17 12:39:04 +0200

    Go to an edit page

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// date
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day}`;
 
 // mongoose set up
-mongoose.connect('mongodb+srv://admin-ssk:e47W66@cluster0.lmjygxq.mongodb.net/DoToDB');
+mongoose.connect('mongodb+srv://admin-ssk:e47W66@cluster0.lmjygxq.mongodb.net/BlogDB');
 
 const blogSchema = new mongoose.Schema({
     header: {
@@ -20,8 +28,7 @@ const blogSchema = new mongoose.Schema({
         required: [true]
     },
     date: {
-        type: Date,
-        default: Date.now
+        type: String,
     },
     paragraph: {
         type: String,
@@ -39,21 +46,19 @@ const Blog = mongoose.model("Blog", blogSchema);
 //Default items
 const defaultBlog = {
     header: "Example Blog Post",
-    date: new Date(), // Current date and time
+    date: formattedDate, // Current date and time
     paragraph: "This is the content of the blog post. It can contain multiple paragraphs and text.",
     author: "John Doe",
 };
 
 const defaultBlog2 = {
     header: "Example Blog Post 2",
-    date: new Date(), // Current date and time
+    date: formattedDate, // Current date and time
     paragraph: "This is the content of the blog post. It can contain multiple paragraphs and text.",
     author: "John Doe",
 };
 
 const defaultBlogs = [defaultBlog, defaultBlog2];
-
-defaultBlog
 
 app.get("/", function(req, res) {
     Blog.find({}).then(function(foundBlogs){
@@ -76,8 +81,73 @@ app.get("/", function(req, res) {
 
   });
 
+
+  // POST
+app.post("/", async function(req, res){
+  
+  const postHeader = req.body.inputHeader;
+  const postParagraph = req.body.inputParagraph;
+  const postAuthor = req.body.inputAuthor;
+
+  console.log(postHeader + "..." + postParagraph + "..." + postAuthor);
+
+  try {
+    const post = new Blog({
+      header: postHeader,
+      date: formattedDate, 
+      paragraph: postParagraph,
+      author: postAuthor,
+  });
+  post.save();
+  res.redirect("/");
+    
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+
+//EDIT
+
+app.post("/edit", async function(req, res){
+  
+  const postID = req.body.edit;
+  const currentPost = await Blog.findById(postID);
+  res.render("partials/edit", {editPost: currentPost});
+
+});
+
+
+// app.post("/",)
+
+
+
+
+
+//DELETE
+app.post("/delete", async function (req, res) {
+
+  const postToDel = req.body.delete;
+  try {
+    await Blog.findByIdAndDelete(postToDel);
+    res.redirect("/");
+  } catch (error) {
+    console.error(`Error deleting document: ${error}`);
+  }
+});
+
+
+
+
+
   app.get("/post", function (req, res) {
     res.render("partials/post");
+  })
+
+  app.get("/edit", function (req, res) {
+    res.render("partials/edit");
   })
 
 app.listen(3000, function() {
